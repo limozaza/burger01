df093d9ee55c184d7d46c341cd5c2464c9149772
limozaza <zakboite@gmail.com>
2018-12-09 12:57:10 +0100

    add and remove dinamicly ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,23 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    let transformIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_,index)=>{
+            return <BurgerIngredient key={igKey + index} type={igKey} />
+        });
+    }).reduce(
+        (arr,el)=> {
+            return arr.concat(el)
+        }, []
+    );
+    if(transformIngredients.length === 0 ){
+        transformIngredients = <p>Please start adding Ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
